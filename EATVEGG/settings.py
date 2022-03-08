b93165a728e66078acbfc4126af0183f8d8c59f7
eatvegg <pythonproj001@gmail.com>
2022-03-09 02:17:17 +0530

    'hfgh'

--- EATVEGG/settings.py
+++ EATVEGG/settings.py
@@ -48,6 +48,7 @@ prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 
+
 INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'accounts.apps.AccountsConfig',
