b48824ef2bd876031efca917ec7826da5983fd22
eatvegg <pythonproj001@gmail.com>
2022-03-09 23:35:26 +0530

    'sd'

--- EATVEGG/urls.py
+++ EATVEGG/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
 
 ]
 
-# urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
